bd11cb1382c3b8031631d0e951159fe147eb7821
Parvin-Gasimli <parvinzga@code.edu.az>
2024-08-05 21:39:56 +0400

    fix(component): formik component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { getPosts, getUsers, urlFor } from "./services/sanity";
+import { createUser, getPosts, getUsers, urlFor } from "./services/sanity";
 
 const App = () => {
   const [data, setData] = useState([]);
   const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    telephone: "",
+    image: "",
+  });
   useEffect(() => {
     getPosts().then((res) => {
       setData(res);
@@ -12,7 +19,32 @@ const App = () => {
       setUser(res);
     });
   }, []);
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setForm({ ...form, [e.target.name]: e.target.files[0] });
+    } else {
+      setForm({ ...form, [e.target.name]: e.target.value });
+    }
+  };
+  const handleSubmit = async () => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("name", form.name);
+    formData.append("email", form.email);
+    formData.append("telephone", form.telephone);
+    formData.append("image", form.image);
+    console.log(Object.entries(formData));
+    await createUser(formData).then((res) => {
+      setUser([...user, res]);
+      setForm(
+        Object.keys(form).reduce((acc, key) => ({ ...acc, [key]: "" }), {})
+      );
+    });
 
+    setLoading(false);
+  };
+
+  console.log(form);
   return (
     <div className="max-w-[1440px] mx-auto">
       <div className="flex flex-wrap gap-2 my-10 items-stretch">
@@ -33,6 +65,46 @@ const App = () => {
               </div>
             </div>
           ))}
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <input
+          className="border p-2 my-2 w-full rounded-md"
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          placeholder="Name"
+        />
+        <input
+          className="border p-2 my-2  w-full rounded-md"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={form.email}
+          placeholder="Email"
+        />
+        <input
+          className="border p-2 my-2  w-full rounded-md"
+          type="text"
+          name="telephone"
+          onChange={handleChange}
+          value={form.telephone}
+          placeholder="Telephone"
+        />
+        <input
+          className="border p-2 my-2  w-full rounded-md"
+          type="file"
+          name="image"
+          onChange={handleChange}
+        />
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-500 text-white p-2"
+          type="submit"
+        >
+          {loading ? "Loading..." : "Submit"}
+        </button>
       </div>
       <div className="flex flex-wrap gap-2 my-10 items-stretch">
         {user &&
--- src/services/sanity.js
+++ src/services/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET,
+  dataset: process.env.REACT_APP_SANITY_DATASET || "production",
   useCdn: true,
   apiVersion: "2023-05-03",
   token: process.env.REACT_APP_SANITY_TOKEN,
@@ -19,6 +19,23 @@ export async function getUsers() {
     '*[_type == "user"]{_id,name,email,telephone,image}'
   );
   return posts;
+}
+export async function createUser(body) {
+  const user = {
+    _type: "user",
+    name: body.get("name"),
+    email: body.get("email"),
+    telephone: body.get("telephone"),
+    image: body.get("image"),
+  };
+
+  const createdUser = await client.create(user, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_SANITY_TOKEN}`,
+    },
+  });
+
+  return createdUser;
 }
 
 const builder = imageUrlBuilder(client);
